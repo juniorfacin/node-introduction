dc45eca5ddf64a45d55a0b13e0173b5deb562eef
Junior Facin <jufacin@hotmail.com>
2025-02-13 06:27:19 -0300

    "exercicio - html escaper - finalizado"

--- 17-projeto-pratico/html-escaper.js
+++ 17-projeto-pratico/html-escaper.js
@@ -2,6 +2,8 @@ const fs = require("node:fs")
 const path = require("node:path")
 const readLine = require("node:readline")
 
+run()
+
 function escapeHtmlSpecialCharacters(text) {
     return text.replace(/[<>&]/g, (match) => {
         switch (match) {
@@ -38,4 +40,35 @@ function askFilePath(question) {
             rl.close()
         })
     })
+}
+
+async function userInteraction() {
+    let inputPath = process.argv[2]
+    if (!inputPath) {
+        inputPath = await askFilePath("Informe o caminho do arquivo de entrada: ")
+    }
+    inputPath = path.resolve(inputPath)
+
+    const defaultName = `escaped_${path.basename(inputPath)}.txt`
+    const answer = await askFilePath(`Informe o caminho do arquivo de saída (padrão: ${defaultName}): `)
+    let outputPath = answer.length > 0 ? answer : defaultName
+    outputPath = path.resolve(outputPath)
+
+    escapeHtmlFile(inputPath, outputPath)
+
+}
+
+function run() {
+    if (process.argv.length >= 4) {
+        escapeHtmlFile(
+            path.resolve(process.argv[2]),
+            path.resolve(process.argv[3])
+        )
+    } else {
+        console.log("---------------------")
+        console.log("HTML Tag Escaper v1.0")
+        console.log("---------------------\n")
+        console.log("Argumentos não informados! Por favor, informe os caminhos dos arquivos para realizar o escape.")
+        userInteraction()
+    }
 }
